1b172212cbbef8b3235d670e31d3990f7a6703d7
Jorge Torres <jortorre@umag.cl>
2017-12-30 21:27:05 -0300

    Add files via upload

---
+++ BulletBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+	public float lifeTime = 3.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime -= Time.deltaTime;
+		if (lifeTime <= 0) {
+			Delete ();
+		}
+	}
+
+	public void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "Target") {
+			TargetBehavior tarScript = col.gameObject.GetComponent<TargetBehavior> (); 
+			tarScript.Impact ();
+			Delete ();
+			Destroy (col.collider);
+		}
+	}
+	void Delete(){
+		Destroy(gameObject);
+	}
+}
